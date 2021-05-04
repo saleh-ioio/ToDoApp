d5e7e79b83af965ab6c8aa7fb7aa423b54a6c23c
Saleh Al Saleh <saleh.alsaleh802@gmail.com>
2021-05-04 23:52:56 +0300

    added circular progress bar package and tried it

--- lib/timer.dart
+++ lib/timer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 
 class timer extends StatelessWidget {
   @override
@@ -16,31 +17,63 @@ class timer extends StatelessWidget {
 }
 
 class timerPage extends StatefulWidget {
-  timerPage({Key key,}) : super(key: key);
+  double progressingTime = 0.6;
+  
+  timerPage({
+    Key key,
+  }) : super(key: key);
   @override
   _timerPageState createState() => _timerPageState();
 }
 
 class _timerPageState extends State<timerPage> {
-
+  CountDownController control = new CountDownController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(
+    return Scaffold(
+      appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Center(
-          child: Text(
-          "its me timer" 
-          ),
+          child: Text("its me timer"),
         ),
       ),
-      body:Text("timer")
-      ,
+      body: Center(
+          child: CircularCountDownTimer(
+        duration: 10,
+        initialDuration: 0,
+        controller: control,
+        width: MediaQuery.of(context).size.width / 2,
+        height: MediaQuery.of(context).size.height / 2,
+        ringColor: Colors.grey[300],
+        ringGradient: null,
+        fillColor: Colors.purpleAccent[100],
+        fillGradient: null,
+        backgroundColor: Colors.transparent,
+        backgroundGradient: null,
+        strokeWidth: 20.0,
+        strokeCap: StrokeCap.round,
+        textStyle: TextStyle(
+            fontSize: 33.0, color: Colors.blue, fontWeight: FontWeight.bold),
+        textFormat: CountdownTextFormat.S,
+        isReverse: false,
+        isReverseAnimation: false,
+        isTimerTextShown: true,
+        autoStart: true,
+        onStart: () {
+          print('Countdown Started');
+        },
+        onComplete: () {
+          print('Countdown Ended');
+        },
+      )),
       floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          control.pause();
+          control.restart();
+        },
         child: Icon(Icons.add),
-        
       ),
     );
-
   }
 }
